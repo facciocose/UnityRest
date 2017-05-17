ba882b14b4f2cb452a6207f9a4c4ed2ce95d2830
Luca Marra <ldm.marra@gmail.com>
2017-05-17 17:46:19 +0200

    client is a monobehaviour

--- UnityRest/Client.cs
+++ UnityRest/Client.cs
@@ -1,30 +1,39 @@
 ﻿using RSG;
 using System;
-using System.Net;
+using System.Collections;
+using UnityEngine;
 
 namespace UnityRest
 {
-    public class Client
+    public class Client: MonoBehaviour
     {
         public IPromise<string> Download(string url)
         {
             var promise = new Promise<string>();
-            using(var client = new WebClient())
-            {
-                client.DownloadStringCompleted += (s, ev) =>
+
+            StartCoroutine(DownloadEnum(url, (text, ex) => {
+                if (ex != null)
                 {
-                    if (ev.Error != null)
-                    {
-                        promise.Reject(ev.Error);
-                    }
-                    else
-                    {
-                        promise.Resolve(ev.Result);
-                    }
-                };
-                client.DownloadStringAsync(new Uri(url), null);
-            }
+                    promise.Reject(ex);
+                } else
+                {
+                    promise.Resolve(text);
+                }
+            }));
             return promise;
+        }
+
+        IEnumerator DownloadEnum(string url, Action<string, Exception> done)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+            if (string.IsNullOrEmpty(www.error))
+            {
+                done(www.text, null);
+            } else
+            {
+                done(null, new Exception(www.error));
+            }
         }
     }
 }
