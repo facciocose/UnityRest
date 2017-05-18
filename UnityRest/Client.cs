6679731c569c4afe8592d736fa60dc0a7e23c50c
Luca Marra <ldm.marra@gmail.com>
2017-05-18 17:07:55 +0200

    added downloadimage func

--- UnityRest/Client.cs
+++ UnityRest/Client.cs
@@ -11,13 +11,30 @@ namespace UnityRest
         {
             var promise = new Promise<string>();
 
-            StartCoroutine(DownloadEnum(url, (text, ex) => {
+            StartCoroutine(DownloadEnum(url, (response, ex) => {
                 if (ex != null)
                 {
                     promise.Reject(ex);
                 } else
                 {
-                    promise.Resolve(text);
+                    promise.Resolve(response);
+                }
+            }));
+            return promise;
+        }
+
+        public IPromise<Texture2D> DownloadImage(string url)
+        {
+            var promise = new Promise<Texture2D>();
+
+            StartCoroutine(DownloadEnumImage(url, (response, ex) => {
+                if (ex != null)
+                {
+                    promise.Reject(ex);
+                }
+                else
+                {
+                    promise.Resolve(response);
                 }
             }));
             return promise;
@@ -31,6 +48,20 @@ namespace UnityRest
             {
                 done(www.text, null);
             } else
+            {
+                done(null, new Exception(www.error));
+            }
+        }
+
+        IEnumerator DownloadEnumImage(string url, Action<Texture2D, Exception> done)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+            if (string.IsNullOrEmpty(www.error))
+            {
+                done(www.texture, null);
+            }
+            else
             {
                 done(null, new Exception(www.error));
             }
